a204f84804753d0c030dc31912e159c40a90437f
JJoaoll <joaoduos@gmail.com>
2025-08-14 16:55:02 -0300

    add: touch-pad-compatibility

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, zen-browser, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          ./modules/configuration.nix
+
+        home-manager.nixosModules.home-manager
+          {
+            home-manager = {  
+              useGlobalPkgs = true;
+              useUserPackages = true;
+
+              extraSpecialArgs = { inherit inputs; system = "x86_64-linux"; };
+
+              users.jjoaoll = import ./modules/home;
+            };
+          }
+        ];
+      };
+    };
+  };
+}
