c3bf07152f3cbaea0208dd9a99714c2205a66e94
JJoaoll <joaoduos@gmail.com>
2025-08-14 22:36:48 -0300

    final stage before restart

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -12,21 +12,36 @@
     # Your settings need to go into the settings attribute set
     # most settings are documented in the appendix
     settings = {
-      vim.viAlias = false;
-      vim.vimAlias = true;
-      vim.lsp = {
-        enable = true;
+      vim = {
+        viAlias = false;
+        vimAlias = true;
+
+        options = {
+          tabstop = 2;
+          shiftwidth = 2;
+        };
+
+
+        
+        statusline.lualine = {
+          enable = true;
+          theme = "gruvbox_dark";
+        };
+
+        lsp = {
+          enable = true;
+        };
+
+        languages = {
+          enableDAP = true;
+          enableExtraDiagnostics = true;
+          enableFormat = true;
+          enableTreesitter = true;
+
+          nix.enable = true; 
+          haskell.enable = true;
+        }; 
       };
-
-      vim.languages = {
-        enableDAP = true;
-        enableExtraDiagnostics = true;
-        enableFormat = true;
-        enableTreesitter = true;
-
-        nix.enable = true; 
-        haskell.enable = true;
-      }; 
     };
   };
 
