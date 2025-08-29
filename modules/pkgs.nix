4fff539c051c0efb3097048201d6b2e157ac3840
JJoaoll <joaoduos@gmail.com>
2025-08-29 10:18:58 -0300

    add: haskell things

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -2,19 +2,25 @@
 
   environment.systemPackages = with pkgs; [
     gleam
+
     ghc
+    cabal-install
+    haskell-language-server
+
+    haskellPackages.cabal2nix
+    haskellPackages.Cabal_3_14_2_0   
+    # haskellPackages.cabal-install
     haskellPackages.SDL
     haskellPackages.SDL-gfx
     haskellPackages.SDL-ttf
     haskellPackages.sdl2
+    haskellPackages.lsp
 
     # beamMinimal28Packages.elixir_1_18
     # beamMinimal28Packages.elixir-ls
     # beamMinimal28Packages.erlang
     # beamMinimal28Packages.erlang-ls
     # beamMinimal28Packages.webdriver
-
-    
   ];
 
 }
